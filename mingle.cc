8f9fb3949fa096c3e40d197b64ff61ed597bbec2
Wat1is <rusgamemovies@gmail.com>
2026-01-22 23:02:02 +0100

    Add files via upload

---
+++ mingle.cc
@@ -0,0 +1,77 @@
+#include <G4VUserDetectorConstruction.hh>
+#include <G4tgbVolumeMgr.hh>
+
+
+class Detector : public G4VUserDetectorConstruction
+{
+	public:
+		G4VPhysicalVolume* Construct() {
+			G4tgbVolumeMgr::GetInstance()->AddTextFile("polydetector.tg");
+			return G4tgbVolumeMgr::GetInstance()->ReadAndConstructDetector();
+		} ///< load detector definition from a text file "detector.tg"
+};
+//....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
+
+#include <G4VUserPrimaryGeneratorAction.hh>
+#include <G4GeneralParticleSource.hh>
+
+class Generator : public G4VUserPrimaryGeneratorAction
+{
+	private:
+		G4GeneralParticleSource* fGPS;
+	public:
+		Generator() : G4VUserPrimaryGeneratorAction() {
+			fGPS = new G4GeneralParticleSource; }
+		~Generator() { delete fGPS; }
+		void GeneratePrimaries(G4Event *evt) { fGPS->GeneratePrimaryVertex(evt); }
+};
+//....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
+
+#include <G4VUserActionInitialization.hh>
+
+class Action : public G4VUserActionInitialization
+{
+	public:
+        void Build() const {
+            SetUserAction(new Generator);
+        }
+};
+//....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
+
+#include <G4RunManagerFactory.hh>
+#include <G4PhysListFactory.hh>
+#include <G4AnalysisManager.hh>
+#include <G4ScoringManager.hh>
+#include <G4VisExecutive.hh>
+#include <G4UIExecutive.hh>
+#include <G4VisManager.hh>
+#include <G4UImanager.hh>
+
+int main(int argc,char** argv)
+{
+	auto *run = G4RunManagerFactory::CreateRunManager();
+
+	G4ScoringManager::GetScoringManager(); // enable macro commands in /score/
+
+
+	G4PhysListFactory factory;
+	run->SetUserInitialization(factory.ReferencePhysList());
+
+	run->SetUserInitialization(new Detector);
+
+	run->SetUserInitialization(new Action);
+
+
+
+	G4VisManager *vis = new G4VisExecutive("quiet"); vis->Initialize();
+
+	if (argc==1) { // interactive mode
+		G4UIExecutive ui(argc, argv);
+		ui.SessionStart();
+	} else { // batch mode
+		G4String command = "/control/execute ";
+		G4UImanager::GetUIpointer()->ApplyCommand(command+argv[1]);
+	}
+
+	delete vis; delete run;
+}
